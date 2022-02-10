98781280b161fc20faf90f4f931b22e3c8a80467
xem <xem.information@gmail.com>
2022-02-11 01:44:01 +0600

    In the name of Allah

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿//In the name of Allah
+
+
+string pwd = Directory.GetCurrentDirectory();
+var dir = Directory.CreateDirectory(pwd + "/demo");
+var demo = dir.FullName + "/demo.txt";
+StreamWriter? file = !File.Exists(demo)? File.CreateText(dir.FullName + "/demo.txt"):null;
+if(file != null ){
+    file.WriteLine("In the name of Allah");
+}//if
